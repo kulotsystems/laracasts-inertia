7caceab29c8ef3fa86c7941f0cd646fba7ba4d7f
kulotsystems <kulotsystems@gmail.com>
2023-04-02 17:04:00 +0800

    Pages.
    Ep 2: Pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Hello, {{ name }}</h1>
+
+    <p>In this series, we will use the following frameworks:</p>
+    <ul>
+        <li v-for="framework in frameworks" :key="framework">{{ framework }}</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        props: {
+            name: String,
+            frameworks: Array
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
