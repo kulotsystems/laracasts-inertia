1b57a1570bd991f9321a1d4b9d5e7bbec4bc81a3
kulotsystems <kulotsystems@gmail.com>
2023-04-04 18:47:15 +0800

    Progress Indicators.
    Ep 5: Progress Indicators

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,16 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import NProgress from 'nprogress';
+import { router } from '@inertiajs/vue3';
+
+
+NProgress.configure({
+    showSpinner: false,
+    template: '<div class="bar" role="bar" style="background: red"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+});
+router.on('start', ()  => NProgress.start());
+router.on('finish', () => NProgress.done());
+
 
 createInertiaApp({
     resolve: name => {
@@ -11,4 +22,5 @@ createInertiaApp({
             .use(plugin)
             .mount(el)
     },
+    progress: false
 });
